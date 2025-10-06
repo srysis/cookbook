5294949c06aa396762ef4a5616cc888a30996a37
srysis <jashandenis@gmail.com>
2025-10-06 14:41:36 +0200

    send basic request via axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+import axios from './api/axios.ts'
+
+if (import.meta.env.PROD) {
+	disableReactDevTools();
+}
 
 function App() {
+	const [recipes, setRecipes] = useState<any>();
 
+	useEffect(() => {
+		axios.get(`/recipes`)
+		.then((response: any) => {
+			setRecipes(response.data.recipes);
+			console.log(response.data.recipes)
+		})
+		.catch((error: any) => {
+			console.error(error);
+		});
+	}, [])
+
+	return(
+		<>
+			{recipes && recipes.map((recipe) => <p>{recipe.ingredients}</p>)}
+		</>
+	)
 }
 
 export default App
