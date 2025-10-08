68b78f11407f0961dfa405ddf3aca5ef00d853e8
srysis <jashandenis@gmail.com>
2025-10-08 15:24:43 +0200

    import a 'cookie-parser' module to allow server to access cookies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dot_env = require('dotenv');
+const cookieparser = require('cookie-parser');
 
 const database = require('./database.js');
 
@@ -10,6 +11,7 @@ const recipesRoutes = require('./routes/recipes.js');
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
+app.use(cookieparser());
 
 dot_env.config();
 
