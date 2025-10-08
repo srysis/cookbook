edd6a59bcf03c432a356c309172e1b8de074ae40
srysis <jashandenis@gmail.com>
2025-10-08 14:44:54 +0200

    use 'localStorage' to store authorization related data; implemented 'login' and 'logoff' functionality; added new routes to 'registartion' and 'login' pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,11 +10,53 @@ import SearchRecipesPage from './pages/search_recipes'
 import AddRecipePage from './pages/add_recipe'
 import RecipePage from './pages/recipe_page'
 
+import RegistrationPage from './pages/auth/registration_page'
+import LoginPage from './pages/auth/login_page'
+
 if (import.meta.env.PROD) {
 	disableReactDevTools();
 }
 
 function App() {
+	const stored_web_token: string | number | null = window.localStorage.getItem('t');
+	const stored_user_ID: string | number | null = window.localStorage.getItem('id');
+
+	let logged_in = null;
+
+	if (stored_web_token && stored_user_ID) {
+		logged_in = true;
+	} else {
+		logged_in = false;
+	}
+
+	const [isLoggedIn, setLoggedInState] = useState<boolean>(logged_in);
+
+	function logIn(login_data: any) {
+		const token = login_data.token;
+		const user_id = login_data.user_id;
+
+		window.localStorage.setItem('t', token);
+		window.localStorage.setItem('id', user_id);
+
+		setLoggedInState(true);
+	}
+
+	function logOut() {
+		if (stored_web_token && stored_user_ID) {
+			axios.post('/auth/logout')
+			.then((response: any) => {
+				if (response.data.success) {
+					setLoggedInState(false);
+
+					window.localStorage.removeItem('t');
+					window.localStorage.removeItem('id');
+
+					window.location.reload();
+				}
+			})
+		}
+	}
+
 	return(
 		<BrowserRouter basename="/">
 			<Routes>
@@ -23,6 +65,9 @@ function App() {
 					<Route path="/search" element={<SearchRecipesPage />} />
 					<Route path="/add_recipe" element={<AddRecipePage />} />
 					<Route path="/recipe/:id" element={<RecipePage />} />
+
+					<Route path="/login" element={<LoginPage isLoggedIn={isLoggedIn} logIn={logIn} />} />
+					<Route path="/register" element={<RegistrationPage isLoggedIn={isLoggedIn} />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
