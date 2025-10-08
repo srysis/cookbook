098791532b4a684657c241f37e2329145942f6ef
srysis <jashandenis@gmail.com>
2025-10-08 14:41:54 +0200

    import and use newly created module files with defined endpoints; defined 'access' and 'refresh' key global vars (values are being pulled from environmental variables)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const dot_env = require('dotenv');
 
 const database = require('./database.js');
 
+const authRoutes = require('./routes/auth.js');
+const recipesRoutes = require('./routes/recipes.js');
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
@@ -11,57 +14,16 @@ app.use(cors({ origin: true, credentials: true }));
 dot_env.config();
 
 global.port = process.env.PORT;
+global.access_key = process.env.ACCESS_KEY;
+global.refresh_key = process.env.REFRESH_KEY;
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api/', recipesRoutes);
 
 // root
 app.get('/', (request, response) => {
 	response.json(`Cookbook API is running. Awaiting instructions...`);
 });
-
-app.get('/api/recipes', (request, response) => {
-	const query = "SELECT * FROM `recipes`";
-
-	database.query(query, (error, data) => {
-		if (error) return response.json(error);
-
-		response.json({recipes: data})
-	})
-});
-
-app.get('/api/recipe/:id', (request, response) => {
-	const recipe_id = request.params.id;
-
-	const get_recipe_query = "SELECT * FROM `recipes` WHERE `id` = " + recipe_id;
-
-	database.query(get_recipe_query, (error, data) => {
-		if (error) return response.json(error);
-
-		response.json({recipe_info: data[0]});
-	})
-})
-
-app.post('/api/recipe', (request, response) => {
-	const { name, description, ingredients } = request.body.recipe_data;
-
-	const add_recipe_query = "INSERT INTO `recipes` (`name`, `description`, `ingredients`) VALUES ('" + name + "', '" + description + "', '" + ingredients + "');"
-
-	database.query(add_recipe_query, (error, data) => {
-		if (error) return response.json(error);
-
-		response.json({recipe_info: data, inserted_ID: data.insertID});
-	})
-})
-
-app.get('/api/filter_recipes', (request, response) => {
-	const ingredients = Object.values(request.query);
-
-	const query = "SELECT * FROM `recipes` WHERE MATCH(`ingredients`) AGAINST ('" + ingredients + "')";
-
-	database.query(query, (error, data) => {
-		if (error) return response.json(error);
-
-		response.json(data);
-	})
-});
-
 
 app.listen(process.env.PORT || port, () => {console.log(`Cookbook API running on port ${port}`)});
