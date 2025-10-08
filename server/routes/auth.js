c777ed90f34e9b735954fbe96f918c4cca8b19eb
srysis <jashandenis@gmail.com>
2025-10-08 15:23:53 +0200

    increased access and refresh token lifetime; added 'verify' for checking if user is still logged in; added 'refresh' endpoint that dispenses a new access token if current one is expired and if possible to do so

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -50,16 +50,16 @@ router.post('/login', (request, response) => {
 			bcrypt.compare(password, data[0].password)
 			.then((result) => {
 				if (result) {
-					const access_token = jwt.sign({ id: data[0].id }, access_key, { expiresIn: '1h' });
+					const access_token = jwt.sign({ id: data[0].id }, access_key, { expiresIn: '12h' });
 
-					const refresh_token = jwt.sign({ id: data[0].username }, refresh_key, { expiresIn: '7d' });
+					const refresh_token = jwt.sign({ id: data[0].username }, refresh_key, { expiresIn: '30d' });
 
 					response.cookie('t', refresh_token, {
 						httpOnly: true,
 						path: '/',
 						secure: true,
 						sameSite: "none",
-						maxAge: 604800000
+						maxAge: 2592000000
 					});
 
 					response.json({success: true, token: access_token, admin: data[0].admin, user_id: data[0].id});
@@ -82,6 +82,74 @@ router.post('/logout', (request, response) => {
 	})
 
 	response.json({success: true, message: "Logged out."});
+})
+
+router.get('/verify/:id', (request, response) => {
+	const token = request.headers['authorization'];
+
+	if (token) {
+		const user_id = request.params.id;
+
+		try {
+			const decoded_token = jwt.verify(token, access_key);
+
+			if (decoded_token.id == user_id) { 
+				let isAdmin;
+
+				const find_user_query = "SELECT `id`,`admin` FROM `users` WHERE `id` = " + decoded_token.id;
+
+				database.query(find_user_query, (error, data) => {
+					if (error) return response.json(error);
+
+					if (data[0].admin == 1) isAdmin = data[0].admin;
+
+					response.json({success: true, message: "User is logged in.", admin: isAdmin});
+				})
+
+			} else {
+				response.status(401).json({success: false, message: "Passed token does not correspond to the passed user ID.", refreshable: false});
+			}
+
+		} catch (error) {
+			if (error.name == "TokenExpiredError") {
+				response.status(401).json({success: false, message: "Passed token has been expired.", refreshable: true});
+			} else {
+				response.status(401).json({success: false, message: "Passed token is either invalid or modified.", refreshable: false});
+			}
+		}
+	} else {
+		response.status(401).json({success: false, message: "User is not logged in."})
+	}
+});
+
+router.post('/refresh/:id', (request, response) => {
+	const user_id = request.params.id;
+
+	if (request.cookies?.t) {
+		const refresh_token = request.cookies.t;
+
+		const find_user_query = "SELECT `username` FROM `users` WHERE `id` = " + user_id;
+
+		try {
+			const decoded_refresh_token = jwt.verify(refresh_token, refresh_key);
+
+			database.query(find_user_query, (error, data) => {
+				if (error) return response.json(error);
+
+				if (data[0].username == decoded_refresh_token.id) {
+					const access_token = jwt.sign({ id: user_id }, access_key, { expiresIn: '12h' });
+
+					response.json({success: true, token: access_token});
+				} else {
+					response.status(401).json({success: false, message: "Passed token is either invalid, modified or expired."});
+				}
+			})
+		} catch (error) {
+			response.status(401).json({success: false, message: "Passed token is either invalid, modified or expired."});
+		}
+	} else {
+		response.status(401).json({success: false, message: "Passed token is either invalid or modified"});
+	}
 })
 
 module.exports = router;
