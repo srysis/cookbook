0a1d9cf546f4a3eba6bd068279358f09240cab80
srysis <jashandenis@gmail.com>
2025-10-08 14:53:35 +0200

    display 'Log In' link if client is not authorized; display 'Log Out' button if client is authorized

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -2,13 +2,28 @@ import { Link } from 'react-router-dom'
 
 import "../../style/layout/header.css"
 
-function Header() {
+interface props {
+	isLoggedIn: boolean,
+	logOut: Function
+}
+
+function Header({isLoggedIn, logOut}: props) {
+	const stored_user_id: any = window.localStorage.getItem('id');
+
 	return(
 		<header>
 			<nav>
+				{!isLoggedIn && 
+					<> 
+						<Link id="log_in_button" to="/login">Log In</Link>
+					</> 
+				}
 				<Link to="/">Home</Link>
 				<Link to="/search">Search recipes</Link>
 				<Link to="/add_recipe">Add a new recipe</Link>
+				{isLoggedIn && 
+					<button onClick={logOut}>Log out</button>
+				}
 			</nav>
 		</header>
 	)
