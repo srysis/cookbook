d009f8dbf3632110fb575f662206b2a570110f63
srysis <jashandenis@gmail.com>
2025-10-09 12:57:57 +0200

    created a middleware for user access validation

---
+++ server/middlewares/authenticator.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+function authenticator(request, response, next) {
+	const token = request.headers['authorization'];
+	const user_id = request.body.user_id;
+	
+	if (token) {
+		try {
+			const decoded_token = jwt.verify(token, access_key);
+
+			if (decoded_token) {
+				if (decoded_token.id == user_id) {
+					next();
+				} else {
+					response.status(401).json({success: false, message: "Passed token does not correspond to the passed user ID."})
+				}
+			}
+		} catch (error) {
+			if (error.name == "TokenExpiredError") {
+				response.status(401).json({success: false, message: "Passed token has been expired.", refreshable: true});
+			} else {
+				response.status(401).json({success: false, message: "Passed token is either invalid or modified.", refreshable: false});
+			}
+		}
+		
+	} else {
+		response.status(401).json({success: false, message: "No authorization header passed."})
+	}
+}
+
+module.exports = authenticator;
