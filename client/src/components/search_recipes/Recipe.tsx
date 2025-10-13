e4dc77ec88e9f50cecd5c2ece91cb889b4c538f8
srysis <jashandenis@gmail.com>
2025-10-13 15:15:42 +0200

    accept 'ingredients_list' string array from props; recipe ingredients that match one of the specified ingredients in the search bar will be marked with a special 'matching_ingredient' class; changes and improvements in HTML structure; implemented the 'capitalize' function that 'capitalizes' passed string value

--- client/src/components/search_recipes/Recipe.tsx
+++ client/src/components/search_recipes/Recipe.tsx
@@ -1,20 +1,49 @@
+import { Link } from 'react-router-dom'
+
 interface props {
 	recipe: {
 		id: number,
 		name: string,
 		description: string,
 		ingredients: string
-	}
+	},
+	ingredients_list: string[]
 }
 
-function Recipe({recipe}: props) {
+function Recipe({recipe, ingredients_list}: props) {
 	const { id, name, description, ingredients } = recipe;
 
+	function capitalize(word: string) {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	}
+
+	let ingredients_HTML = [];
+
 	return(
-		<div className="recipe">
-			<p>{name}</p>
-			<p>{ingredients}</p>
-		</div>
+		<>
+			{ingredients.split(/[ ,]+/).map((recipe_ingredient, index) => {
+				if (ingredients_list.includes(recipe_ingredient)) {
+					ingredients_HTML.push(<span key={index} className="matching_ingredient">{capitalize(recipe_ingredient)}</span>)
+				}
+			})}
+			{ingredients.split(/[ ,]+/).map((recipe_ingredient, index) => {
+				if (!ingredients_list.includes(recipe_ingredient)) {
+					ingredients_HTML.push(<span key={index}>{capitalize(recipe_ingredient)}</span>)
+				}
+			})}
+			<div className="recipe">
+				<div className="link_container">
+					<div className="overlay">
+						<Link to={`/recipe/${id}`}></Link>
+					</div>
+					<div className="name"><h2>{name}</h2></div>
+					<div className="description">
+						<p>{description ? description : "no description"}</p>
+					</div>
+					<div className="ingredients">{ingredients_HTML}</div>
+				</div>
+			</div>
+		</>
 	)
 }
 
