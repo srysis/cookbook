a5752845c0d67bf6a2a64674825ccf207332f532
srysis <jashandenis@gmail.com>
2025-10-13 15:18:33 +0200

    removed 'onChangeHandler' function and any references to it; changed 'onSubmitHandler' function to make a 'filter_recipes' API call as a callback function for updating the 'ingredients_list' state value to avoid unneccessary rerenders when a new ingredient is being put inside of it; renamed 'ingredients' state into 'ingredients_list'; import a new stylesheet file

--- client/src/pages/search_recipes.tsx
+++ client/src/pages/search_recipes.tsx
@@ -4,31 +4,36 @@ import axios from '../api/axios'
 
 import Recipe from "../components/search_recipes/Recipe"
 
+import "../style/search_page.css"
+
 function SearchRecipes() {
 	const [filtered_recipes, setFilteredRecipes] = useState<any>();
-	const [ingredients, setIngredients] = useState<any>();
+	const [ingredients_list, setIngredientsList] = useState<any>();
 
 	const [isSearchFinished, setIsSearachFinished] = useState<boolean>(false);
-
-	function onChangeHandler(event) {
-		setIngredients(event.target.value);
-	}
 
 	function onSubmitHandler(event: any) {
 		event.preventDefault();
 
-		let query = new URLSearchParams(Object.assign({}, ingredients.split(",")));
-		let queryString = query.toString();
+		setIngredientsList(() => {
+			const new_ingredients_list = (event.target.elements[0].value);
 
-		axios.get(`/filter_recipes?${queryString}`)
-		.then((response: any) => {
-			console.log(response.data);
-			setFilteredRecipes(response.data);
+			if (new_ingredients_list) {
+				let query = new URLSearchParams(Object.assign({}, new_ingredients_list.split(",")));
+				let queryString = query.toString();
 
-			setIsSearachFinished(true);
-		})
-		.catch((error: any) => {
-			console.error(error);
+				axios.get(`/filter_recipes?${queryString}`)
+				.then((response: any) => {
+					setFilteredRecipes(response.data);
+
+					setIsSearachFinished(true);
+				})
+				.catch((error: any) => {
+					console.error(error);
+				})
+			}
+
+			return new_ingredients_list;
 		})
 	}
 
@@ -36,14 +41,14 @@ function SearchRecipes() {
 		<section id="search">
 			<h1>Search</h1>
 			<form onSubmit={onSubmitHandler}>
-				<input type="text" autoComplete="off" onChange={onChangeHandler} />
+				<input type="text" autoComplete="off" />
 				<button>Submit</button>
 			</form>
 			{ isSearchFinished && 
 				<section id="matching_recipes">
 					{filtered_recipes.length > 0 &&
 						<>
-							{filtered_recipes.map((recipe) => <Recipe key={recipe.id} recipe={recipe} />)}
+							{filtered_recipes.map((recipe) => <Recipe key={recipe.id} recipe={recipe} ingredients_list={ingredients_list.split(/[ ,]+/)} />)}
 						</>
 					}
 				</section>
