c4ce9246ea2a9feac055f459118389bfcf993c04
srysis <jashandenis@gmail.com>
2025-10-15 14:54:37 +0200

    create a new 'isLoginPopupVisible' state that keeps track of the 'login popup' visiblity and pass the wrapper function that can change it's value to 'Header' and newly created 'LogInPopup' component

--- client/src/components/layout/Base.tsx
+++ client/src/components/layout/Base.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Header from "./Header"
+import Header from "./Header.tsx"
+import LogInPopup from "./auth/LogInPopup.tsx"
 
 interface props {
 	isLoggedIn: boolean,
+	logIn: Function,
 	logOut: Function
 }
 
-function Base({isLoggedIn, logOut}: props) {
+function Base({isLoggedIn, logIn, logOut}: props) {
+	const [isLoginPopupVisible, setLoginPopupVisible] = useState<boolean>(false);
+
+	function setLoginPopupVisibleWrapper(value: boolean) {
+		setLoginPopupVisible(value);
+	}
+
 	return(
 		<>
-			<Header isLoggedIn={isLoggedIn} logOut={logOut} />
+			<Header isLoggedIn={isLoggedIn} logOut={logOut} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
+			{ !isLoggedIn && isLoginPopupVisible && 
+				<LogInPopup logIn={logIn} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
+			}
 			<main>
 				<Outlet />
 			</main>
