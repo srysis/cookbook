72c6b10c6d2243d449e9ee6a1bdf14b3f5881faf
srysis <jashandenis@gmail.com>
2025-10-21 12:57:57 +0200

    implemented a 'add ingredient' button inside 'ingredients' field; code refactory: removed 'recipe_data' state and replaced with 'recipe_name' and 'recipe_description' states, renamed 'onChangeHandler' into 'onNameOrDescriptionChangeHandler' and adjusted all references to it, 'clearFields' (for now) affects only 'Name' and 'Description' values and fields, 'onSubmitHandler' now constructs a 'recipe_ingredients' array before sending an API request to create a recipe, changed the 'body' of 'POST' request to '/recipe'; changes in layout: second 'div.input_container' is now wrapped inside 'div.input_wrapper'; 'clear' button's status now depends on 'recipe_name' and a new 'doIngredientsExist' state that defines whether at least one ingredient was added;

--- client/src/pages/add_recipe.tsx
+++ client/src/pages/add_recipe.tsx
@@ -8,29 +8,70 @@ import "../style/add_recipe_page.css"
 function AddRecipe() {
 	const navigate = useNavigate();
 
-	const [recipe_data, setRecipeData] = useState<any>({name: "", description: "", ingredients: ""});
+	const [recipe_name, setRecipeName] = useState<string>("");
+	const [recipe_description, setRecipeDescription] = useState<string>("");
+
+	const [doIngredientsExist, setIngredientsExist] = useState<boolean>(false);
+
+	function addInputField(event: any) {
+		const input_field_HTML: HTMLInputElement = document.createElement('input');
+		input_field_HTML.setAttribute("type", "text");
+		input_field_HTML.classList.add("ingredient");
+		input_field_HTML.setAttribute("placeholder", "e.g. cheese, bread, etc.");
+
+		event.target.parentElement.insertBefore(input_field_HTML, document.querySelector("button#add_ingredient")).focus();
+
+		if (!doIngredientsExist) {
+			setIngredientsExist(true);
+		}
+	}
 
 	function clearFields() {
 		const input_fields = document.querySelectorAll("input[type='text'], textarea") as any;
 
 		for (let input_field of input_fields) {
 			input_field.value = "";
-
-			setRecipeData({name: "", description: "", ingredients: ""});
 		}
+
+		setRecipeName("");
+		setRecipeDescription("");
 	}
 
-	function onChangeHandler(event: any) {
-		setRecipeData({
-			...recipe_data,
-			[event.target.id]: event.target.value
-		})
+	function onNameOrDescriptionChangeHandler(event: any) {
+		switch (event.target.id) {
+			case "name":
+				setRecipeName(event.target.value);
+				break;
+			case "description":
+				setRecipeDescription(event.target.value);
+				break;
+			default:
+				console.error("Unexpected value");
+				break;
+		}
 	}
 
 	function onSubmitHandler(event: any) {
 		event.preventDefault();
+		
+		const input_fields = document.querySelectorAll("input.ingredient");
 
-		axios.post(`/recipe`, {user_id: window.localStorage.getItem("id"), recipe_data: recipe_data})
+		if (input_fields.length == 0 || input_fields[0].value === "") {
+			return;
+		}
+
+		let recipe_ingredients: string[] = [];
+
+		for (let input_field of input_fields) {
+			if (input_field.value !== "") recipe_ingredients.push(input_field.value);
+		}
+
+
+		axios.post(`/recipe`, {user_id: window.localStorage.getItem("id"), recipe_data: {
+			name: recipe_name,
+			description: recipe_description,
+			ingredients: recipe_ingredients
+		}})
 		.then((response: any) => {
 			if (response.data.success) {
 				navigate(`/recipe/${response.data.inserted_ID}`)
@@ -48,11 +89,13 @@ function AddRecipe() {
 			<form onSubmit={onSubmitHandler}>
 				<div className="input_container">
 					<label htmlFor="name"><span>Recipe name</span></label>
-					<input type="text" id="name" placeholder="Name your recipe" onChange={onChangeHandler} autoComplete="off" required />
+					<input type="text" id="name" placeholder="Name your recipe" onChange={onNameOrDescriptionChangeHandler} autoComplete="off" required />
 				</div>
-				<div className="input_container">
-					<label htmlFor="ingredients"><span>Ingredients that are required in the recipe</span></label>
-					<input type="text" id="ingredients" placeholder="e.g. cheese, bread, etc." onChange={onChangeHandler} autoComplete="off" required />
+				<div className="input_wrapper">
+					<label><span>Ingredients</span></label>
+					<div className="input_container">
+						<button type="button" id="add_ingredient" onClick={addInputField}>+ Add ingredient</button>
+					</div>
 				</div>
 				<div className="input_container">
 					<label htmlFor="description"><span>Additional information(optional)</span></label>
@@ -62,12 +105,12 @@ function AddRecipe() {
 						rows={4} 
 						cols={40} 
 						maxLength={5000} 
-						onChange={onChangeHandler} 
+						onChange={onNameOrDescriptionChangeHandler} 
 					/>
 				</div>
 				<div className="buttons_container">
 					<button type="button" onClick={() => clearFields()}>Clear</button>
-					<button type="submit" disabled={!recipe_data.name || !recipe_data.ingredients}>Add recipe</button>
+					<button type="submit" disabled={!recipe_name || !doIngredientsExist}>Add recipe</button>
 				</div>
 			</form>
 		</section>
