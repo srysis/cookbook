2c9d0cbc20cf05281ddbf0636eb750623d914328
srysis <jashandenis@gmail.com>
2025-10-27 12:14:16 +0100

    implemented the 'delete recipe' button: create a new 'deleteRecipe' function that makes an API request to delete the specified recipe, the button will be visible only if current user owns this recipe, create a 'popup' that will be shown if the 'delete' button is pressed to ask user's permission to confirm or discard the action; changes in HTML structure

--- client/src/pages/recipe_page.tsx
+++ client/src/pages/recipe_page.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 import { useParams } from 'react-router-dom'
 
 import axios from '../api/axios'
+
+import delete_icon from "../assets/trash_can.png"
 
 import "../style/recipe_page.css"
 
@@ -13,15 +16,23 @@ type Recipe = {
 }
 
 function RecipePage() {
+	const navigate = useNavigate();
+
 	const { id } = useParams();
 
 	const [recipe_info, setRecipeInfo] = useState<Recipe>({id: 0, name: "", description: "", ingredients: ""});
 	const [infoFetched, setInfoFetched] = useState<boolean>(false);
 
+	const [ownership, setOwnership] = useState<boolean>(false);
+
+	const [isDeletePopupVisible, showDeletePopup] = useState<boolean>(false);
+
 	useEffect(() => {
 		axios.get(`/recipe/${id}`)
 		.then((response: any) => {
 			setRecipeInfo(response.data.recipe_info);
+
+			setOwnership(response.data.ownership);
 
 			setInfoFetched(true);
 		})
@@ -29,6 +40,29 @@ function RecipePage() {
 			console.error(error);
 		})
 	}, [])
+
+	async function deleteRecipe(event: any) {
+		event.preventDefault();
+
+		try {
+			const delete_response = await axios.delete(`/recipe/${id}`, {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					user_id: window.localStorage.getItem("id")
+				}
+			});
+
+			if (delete_response.data.success) {
+				navigate('/');
+			} else {
+				navigate(`/recipe/${id}`);
+			}
+		} catch (error: any) {
+			console.error(error);
+		}
+	}
 
 	if (infoFetched) {
 		const { name, description, ingredients } = recipe_info;
@@ -49,14 +83,36 @@ function RecipePage() {
 		
 
 		return(
-			<section id="recipe">
-				<div className="name"><h1>{name}</h1></div>
-				<div className="ingredients">{matching_ingredients_HTML}{non_matching_ingredients_HTML}</div>
-				<hr />
-				<div className="description">
-					<p>{description ? description : "no description"}</p>
-				</div>
-			</section>
+			<>
+				{ isDeletePopupVisible && 
+					<div className="delete_popup_overlay" onClick={(event) => {if (event.target.classList.contains("delete_popup_overlay")) showDeletePopup(false) }}>
+						<div className="delete_popup_container">
+							<h1>Are you sure?</h1>
+							<div className="selection_container">
+								<button type="button" onClick={deleteRecipe}>Yes</button>
+								<button type="button" onClick={() => {showDeletePopup(false)}}>No</button>
+							</div>
+						</div>
+					</div>
+				}
+				<section id="recipe">
+					<div className="recipe_wrapper">
+						<div className="main_info">
+							<div className="name"><h1>{name}</h1></div>
+							<div className="ingredients">{matching_ingredients_HTML}{non_matching_ingredients_HTML}</div>
+						</div>
+						{ ownership && 
+							<div className="delete">
+								<button type="button" onClick={() => {showDeletePopup(true)}}><img src={delete_icon} /></button>
+							</div>
+						}
+					</div>
+					<hr />
+					<div className="description">
+						<p>{description ? description : "no description"}</p>
+					</div>
+				</section>
+			</>
 		)
 	}
 }
