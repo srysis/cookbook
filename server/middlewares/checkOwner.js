4106a111a641f5c873c390fd79088e74cb7c3078
srysis <jashandenis@gmail.com>
2025-10-27 11:13:06 +0100

    create a middleware for checking the recipe owner

---
+++ server/middlewares/checkOwner.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const database = require('../database.js');
+
+function checkOwner(request, response, next) {	
+	const token = request.headers['authorization'];
+	const recipe_id = request.params.id;
+
+	const select_recipe_query = "SELECT * FROM `recipes` WHERE `id` = " + recipe_id;
+
+	database.query(select_recipe_query, (error, data) => {
+		if (error) return response.json(error);
+
+		if (data.length) {
+			try {
+				const decoded_token = jwt.verify(token, access_key);
+
+				if (decoded_token.id == data[0].made_by) {
+					response.locals.ownership = true;
+				} else {
+					response.locals.ownership = false;
+				}
+			} catch (error) {
+				if (error.name == "TokenExpiredError") {
+					return response.status(401).json({success: false, message: "Passed token has been expired.", refreshable: true});
+				} else {
+					response.locals.ownership = false;
+				}
+			}
+		} else {
+			response.locals.ownership = false;
+		}
+		
+		next();
+	});
+}
+
+module.exports = checkOwner;
