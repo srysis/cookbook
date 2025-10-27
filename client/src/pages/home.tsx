014e3b2161eeb6bd52e842d2ee74e5c27459bf36
srysis <jashandenis@gmail.com>
2025-10-27 14:06:32 +0100

    display 'loading spinner' while data is being fetched: added few new states to control the 'loading' process

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,6 +2,7 @@ import axios from '../api/axios'
 import { useState, useEffect } from 'react'
 
 import Recipe from "../components/home/Recipe"
+import LoadingSpinner from "../components/LoadingSpinnerBlock.tsx"
 
 import "../style/home.css"
 
@@ -14,22 +15,37 @@ type Recipe = {
 
 function Home() {
 	const [recipes, setRecipes] = useState<Array<Recipe>>([]);
+	const [areRecipesRetrieved, setRecipesRetrieved] = useState<boolean>(false);
+
+	const [isLoading, setLoadingState] = useState<boolean>(true);
 
 	useEffect(() => {
+		setRecipesRetrieved(false);
+		setLoadingState(true);
+
 		axios.get(`/recipes`)
 		.then((response: any) => {
 			setRecipes(response.data.recipes);
+			setRecipesRetrieved(true);
+
+			setLoadingState(false);
 		})
 		.catch((error: any) => {
 			console.error(error);
 		});
 	}, []);
 
-	return(
-		<section id="home">
-			{recipes && recipes.map((recipe) => <Recipe key={recipe.id} content={recipe} />)}
-		</section>
-	)
+	if (areRecipesRetrieved && !isLoading && recipes != undefined) {
+		return(
+			<section id="home">
+				{recipes && recipes.map((recipe) => <Recipe key={recipe.id} content={recipe} />)}
+			</section>
+		)
+	} else if (isLoading) {
+		return(
+			<LoadingSpinner />
+		)
+	}
 }
 
 export default Home;
