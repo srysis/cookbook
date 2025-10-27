d3f64510ad190abb5f0a821ab9fd7394f61171ce
srysis <jashandenis@gmail.com>
2025-10-27 11:14:44 +0100

    import 'jwt' module; import 'checkOwner' custom middleware; create a new '/recipe/:id' DELETE request that deletes the recipe with the specified ID from the database table

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const database = require('../database.js');
 
-const authenticator = require('../middlewares/authenticator.js')
+const authenticator = require('../middlewares/authenticator.js');
+const checkOwner = require('../middlewares/checkOwner.js');
 
 const router = express.Router();
 
@@ -41,6 +43,49 @@ router.post('/recipe', authenticator, (request, response) => {
 
 		response.json({success: true, inserted_ID: data.insertId});
 	})
+})
+
+router.delete('/recipe/:id', checkOwner, (request, response) => {
+	const token = request.headers['authorization'];
+
+	if (token) {
+		const recipe_id = request.params.id;
+		const user_id = request.body.user_id;
+
+		const select_recipe_query = "SELECT * FROM `recipes` WHERE `id` = " + recipe_id;
+
+		database.query(select_recipe_query, (error, data) => {
+			if (error) return response.status(500).json({success: false, message: "Something went wrong."});
+
+			if (!data.length) {
+				return response.status(404).json({success: false, message: "Specified recipe does not exist."})
+			}
+
+			if (response.locals.ownership) {
+				try {
+					const decoded_token = jwt.verify(token, access_key);
+
+					if (decoded_token.id == user_id) {
+						const delete_recipe_query = "DELETE FROM `recipes` WHERE `id` = '" + recipe_id + "' AND `made_by` = '" + user_id + "'";
+
+						database.query(delete_recipe_query, (error, data) => {
+							if (error) return response.status(500).json({success: false, message: "Something went wrong."});
+
+							response.json({success: true, message: "Recipe was successfully deleted."});
+						})
+					} else {
+						response.status(403).json({success: false, message: "Passed token does not correspond to the passed user ID."});
+					}
+				} catch (error) {
+					response.status(403).json({success: false, message: "Passed token is either invalid, modified or expired."});
+				}
+			} else {
+				response.status(403).json({success: false, message: "Client is not allowed to delete this recipe."});
+			}
+		})
+	} else {
+		response.status(401).json({success: false, message: "No authorization header passed."});
+	}
 })
 
 router.get('/filter_recipes', (request, response) => {
