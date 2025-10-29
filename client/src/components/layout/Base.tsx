c30c0c5c724d7c3dcd63ba1c3006b00ba6fc8935
srysis <jashandenis@gmail.com>
2025-10-29 12:34:39 +0100

    accept 'notification' related 'props' from 'App' component, added a conditional render block that will render 'Notification' component if 'notification_visible' is set to true inside 'App' component

--- client/src/components/layout/Base.tsx
+++ client/src/components/layout/Base.tsx
@@ -2,15 +2,23 @@ import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Header from "./Header.tsx"
+
 import LogInPopup from "./auth/LogInPopup.tsx"
+
+import Notification from "../Notification.tsx"
 
 interface props {
 	isLoggedIn: boolean,
 	logIn: Function,
-	logOut: Function
+	logOut: Function,
+	notification_visible: boolean,
+	notification_message: string,
+	notification_type: string,
+	setNotificationMessage: Function,
+	setNotificationType: Function
 }
 
-function Base({isLoggedIn, logIn, logOut}: props) {
+function Base({isLoggedIn, logIn, logOut, notification_visible, notification_message, notification_type, setNotificationMessage, setNotificationType}: props) {
 	const [isLoginPopupVisible, setLoginPopupVisible] = useState<boolean>(false);
 
 	useEffect(() => {
@@ -30,6 +38,9 @@ function Base({isLoggedIn, logIn, logOut}: props) {
 			<Header isLoggedIn={isLoggedIn} logOut={logOut} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
 			{ !isLoggedIn && isLoginPopupVisible && 
 				<LogInPopup logIn={logIn} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
+			}
+			{notification_visible && 
+				<Notification message={notification_message} type={notification_type} setNotificationMessage={setNotificationMessage} setNotificationType={setNotificationType} />
 			}
 			<main>
 				<Outlet />
