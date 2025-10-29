5704483151b09c0c6d703fddce58add6d4c553e0
srysis <jashandenis@gmail.com>
2025-10-29 12:30:00 +0100

    create a new 'Notification' component for displaying website wide messages such as errors

---
+++ client/src/components/Notification.tsx
@@ -0,0 +1,28 @@
+import acknowledge_icon from "../assets/success-icon.png"
+import error_icon from "../assets/error-icon.png"
+
+import "../style/notification.css"
+// import "../style/mobile/notification.css"
+
+interface props {
+	message: string,
+	type: string,
+	setNotificationMessage: Function, 
+	setNotificationType: Function
+}
+
+function Notification({message, type, setNotificationMessage, setNotificationType}: props) {
+	return (
+		<div id="notification" className={type === "success" ? "success" : "error"}>
+			<div className="overlay" onClick={() => { setNotificationMessage(""); setNotificationType("") }}></div>
+			<div className="icon_container">
+				<img src={type === "success" ? acknowledge_icon : error_icon} />
+			</div>
+			<div className="text_container">
+				<p>{message}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Notification;
