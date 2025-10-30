f20c362984f5113f58b4e7859afcd44e2b476090
srysis <jashandenis@gmail.com>
2025-10-30 11:50:27 +0100

    removed 'description' value from 'recipe' destructurization

--- client/src/components/search_recipes/Recipe.tsx
+++ client/src/components/search_recipes/Recipe.tsx
@@ -12,7 +12,7 @@ interface props {
 }
 
 function Recipe({recipe, ingredients_list}: props) {
-	const { id, name, description, short_description, ingredients } = recipe;
+	const { id, name, short_description, ingredients } = recipe;
 
 	function capitalize(word: string) {
 		return word.charAt(0).toUpperCase() + word.slice(1);
