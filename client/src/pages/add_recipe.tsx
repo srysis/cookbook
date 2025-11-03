80c03a708595489d9b277d8041926a8049a2c6e4
srysis <jashandenis@gmail.com>
2025-11-03 16:59:28 +0100

    import new stylesheet file for mobile

--- client/src/pages/add_recipe.tsx
+++ client/src/pages/add_recipe.tsx
@@ -6,6 +6,7 @@ import axios from '../api/axios.ts'
 import LoadingSpinner from "../components/LoadingSpinnerInline.tsx"
 
 import "../style/add_recipe_page.css"
+import "../style/mobile/add_recipe_page.css"
 
 interface props {
 	setNotificationMessage: Function,
