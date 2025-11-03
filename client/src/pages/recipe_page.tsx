c67a3bf7eae8d32edff4cfb92907a2c0e13b0f35
srysis <jashandenis@gmail.com>
2025-11-03 11:20:22 +0100

    import new stylesheet file for mobile

--- client/src/pages/recipe_page.tsx
+++ client/src/pages/recipe_page.tsx
@@ -10,6 +10,7 @@ import LoadingSpinnerInline from "../components/LoadingSpinnerInline.tsx"
 import delete_icon from "../assets/trash_can.png"
 
 import "../style/recipe_page.css"
+import "../style/mobile/recipe_page.css"
 
 type Recipe = {
 	id: number,
