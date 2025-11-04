f7c49962b7ea6df45c1ad3bcfbc8055276046d85
srysis <jashandenis@gmail.com>
2025-11-04 11:27:08 +0100

    add a 'isAsideVisible' state to determine whether 'Aside' is visible or not; modified 'useEffect' to also hide scrollbar if 'Aside' is visible; added a conditional render block that will render 'Aside' component if 'isAsideVisible' is true; adjusted 'LogInPopup' component rendering condition to also check for 'DEVICE_TYPE' to prevent 'login popup' from 'rendering' on anything that is not 'desktop'

--- client/src/components/layout/Base.tsx
+++ client/src/components/layout/Base.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Header from "./Header.tsx"
+import Aside from "./Aside.tsx"
 
 import LogInPopup from "./auth/LogInPopup.tsx"
 
 import Notification from "../Notification.tsx"
 
 interface props {
+	DEVICE_TYPE: string,
 	isLoggedIn: boolean,
 	logIn: Function,
 	logOut: Function,
@@ -18,25 +20,32 @@ interface props {
 	setNotificationType: Function
 }
 
-function Base({isLoggedIn, logIn, logOut, notification_visible, notification_message, notification_type, setNotificationMessage, setNotificationType}: props) {
+function Base({DEVICE_TYPE, isLoggedIn, logIn, logOut, notification_visible, notification_message, notification_type, setNotificationMessage, setNotificationType}: props) {
+	const [isAsideVisible, toggleAside] = useState<boolean>(false);
+
 	const [isLoginPopupVisible, setLoginPopupVisible] = useState<boolean>(false);
 
 	useEffect(() => {
-		if (isLoginPopupVisible) {
+		if (isAsideVisible || isLoginPopupVisible) {
 			document.body.style.overflow = "hidden";
 		} else {
 			document.body.style.overflow = "";
 		}
-	}, [isLoginPopupVisible])
+	}, [isAsideVisible, isLoginPopupVisible])
 
 	function setLoginPopupVisibleWrapper(value: boolean) {
 		setLoginPopupVisible(value);
 	}
 
+	function toggleAsideWrapper(value: boolean) {
+		toggleAside(value);
+	}
+
 	return(
 		<>
-			<Header isLoggedIn={isLoggedIn} logOut={logOut} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
-			{ !isLoggedIn && isLoginPopupVisible && 
+			{ isAsideVisible && <Aside toggleAside={toggleAsideWrapper} visibility={isAsideVisible} isLoggedIn={isLoggedIn} logOut={logOut} /> }
+			<Header DEVICE_TYPE={DEVICE_TYPE} isLoggedIn={isLoggedIn} logOut={logOut} setLoginPopupVisible={setLoginPopupVisibleWrapper} toggleAside={toggleAsideWrapper} />
+			{ !isLoggedIn && isLoginPopupVisible && DEVICE_TYPE === "desktop" &&
 				<LogInPopup logIn={logIn} setLoginPopupVisible={setLoginPopupVisibleWrapper} />
 			}
 			{notification_visible && 
