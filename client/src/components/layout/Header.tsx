85ef0f78f37fcc9a40a2eb1b361a8ba5056f8a59
srysis <jashandenis@gmail.com>
2025-11-04 11:29:34 +0100

    added conditional rendering blocks that will change the 'Header' of the website depending on current 'DEVICE_TYPE' value

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,29 +1,42 @@
 import { Link } from 'react-router-dom'
 
+import bars_solid from "../../assets/bars-solid.png"
+
 import "../../style/layout/header.css"
+import "../../style/mobile/layout/header.css"
 
 interface props {
+	DEVICE_TYPE: string,
 	isLoggedIn: boolean,
 	logOut: Function,
-	setLoginPopupVisible: Function
+	setLoginPopupVisible: Function,
+	toggleAside: Function
 }
 
-function Header({isLoggedIn, logOut, setLoginPopupVisible}: props) {
+function Header({DEVICE_TYPE, isLoggedIn, logOut, setLoginPopupVisible, toggleAside}: props) {
 	return(
 		<header>
+			{ DEVICE_TYPE === "mobile" && 
+				<div id="aside_toggler">
+					<button type="button" onClick={() => {toggleAside(true)}}><img src={bars_solid} /></button>
+				</div>
+			}
 			<div className="logo_container"><Link to="/">cookbook</Link></div>
-			<nav>
-				<span><Link to="/search">Search recipes</Link></span>
-				{isLoggedIn &&
-					<>
-						<span><Link to="/add_recipe">Add a new recipe</Link></span>
-						<button className="log_out_button" onClick={() => logOut()}>Log out</button>
-					</>
-				}
-				{!isLoggedIn && 
-					<button className="log_in_button" onClick={() => setLoginPopupVisible(true)}>Log in</button>
-				}
-			</nav>
+			{ DEVICE_TYPE === "desktop" &&
+				<nav>
+					<span><Link to="/search">Search recipes</Link></span>
+					{isLoggedIn &&
+						<>
+							<span><Link to="/add_recipe">Add a new recipe</Link></span>
+							<button className="log_out_button" onClick={() => logOut()}>Log out</button>
+						</>
+					}
+					{!isLoggedIn && 
+						<button className="log_in_button" onClick={() => setLoginPopupVisible(true)}>Log in</button>
+					}
+				</nav>
+			}
+			
 		</header>
 	)
 }
