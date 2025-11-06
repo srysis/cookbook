3b32e80f15659b0abcd504cfe302ed71d7737958
srysis <jashandenis@gmail.com>
2025-11-06 15:07:34 +0100

    import 'i18n' config file to use localization functionality

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.tsx'
 
+import './tools/i18n.ts'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
