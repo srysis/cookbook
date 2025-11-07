d449548923af3bf00c2452565f7202b8b31e068f
srysis <jashandenis@gmail.com>
2025-11-07 12:10:52 +0100

    replaced 't('addRecipe')' with 't('addRecipe.title')'

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -35,7 +35,7 @@ function Header({DEVICE_TYPE, isLoggedIn, logOut, setLoginPopupVisible, toggleAs
 					<span><Link to="/search">{t('search.title')}</Link></span>
 					{isLoggedIn &&
 						<>
-							<span><Link to="/add_recipe">{t('addRecipe')}</Link></span>
+							<span><Link to="/add_recipe">{t('addRecipe.title')}</Link></span>
 							<button className="log_out_button" onClick={() => logOut()}>{t('header.logOut')}</button>
 						</>
 					}
