889750f2b78ff73f48621868c8f99b284738cfb4
srysis <jashandenis@gmail.com>
2025-11-11 11:54:45 +0100

    created a new 'layout' component that will be displayed if current user is not logged in

---
+++ client/src/components/layout/Welcome.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { Outlet } from 'react-router-dom'
+
+interface props {
+	isLoggedIn: boolean
+}
+
+function WelcomeLayout({isLoggedIn}: props) {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			navigate("/recipes");
+		}
+	}, [])
+
+	return(
+		<>
+			<main>
+				<Outlet />
+			</main>
+		</>
+	)
+}
+
+export default WelcomeLayout;
