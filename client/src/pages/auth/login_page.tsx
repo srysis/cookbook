ddea73b973a18d4fa602ad9a8676b170ad96d83c
srysis <jashandenis@gmail.com>
2025-11-12 11:50:03 +0100

    import a new stylesheet file

--- client/src/pages/auth/login_page.tsx
+++ client/src/pages/auth/login_page.tsx
@@ -10,6 +10,7 @@ import LoadingSpinner from "../../components/LoadingSpinnerInline.tsx"
 import error_icon from "../../assets/exclamation-mark-2.png"
 
 import "../../style/auth/login_page.css"
+import "../../style/mobile/auth/login_page.css"
 
 type UserCredentials = {
 	username: string,
