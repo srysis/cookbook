1f4f9c4125f13c2d7743104001cc15e8241880bd
srysis <jashandenis@gmail.com>
2025-11-14 14:11:17 +0100

    import a new stylesheet file for tablets

--- client/src/pages/search_recipes.tsx
+++ client/src/pages/search_recipes.tsx
@@ -7,6 +7,7 @@ import Recipe from "../components/search_recipes/Recipe"
 import LoadingSpinner from "../components/LoadingSpinnerBlock.tsx"
 
 import "../style/search_page.css"
+import "../style/tablet/search_page.css"
 import "../style/mobile/search_page.css"
 
 type Recipe = {
